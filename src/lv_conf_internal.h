16c31cbce5fa3cd34cc6203bec647bac3817bdd4
C47D <carlos.santiago.diaz@gmail.com>
2020-10-22 23:37:07 -0500

    Checking for LV_CONF_SKIP so we don't try to add lv_conf.h

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -12,10 +12,11 @@
 
 #if defined ESP_PLATFORM
 #include "sdkconfig.h"
+#include "esp_attr.h"
 #endif
 
 #if defined __has_include
-#  if __has_include("lv_conf.h")
+#  if __has_include("lv_conf.h") && !defined (LV_CONF_SKIP)
 #ifndef LV_CONF_INCLUDE_SIMPLE
 #    define LV_CONF_INCLUDE_SIMPLE
 #endif
@@ -23,7 +24,7 @@
 #endif
 
 /*If lv_conf.h is not skipped include it*/
-#if !defined(LV_CONF_SKIP) && !defined(CONFIG_LV_CONF_SKIP)
+#if !defined(LV_CONF_SKIP) || !defined(CONFIG_LV_CONF_SKIP)
 #  if defined(LV_CONF_PATH)											/*If there is a path defined for lv_conf.h use it*/
 #    define __LV_TO_STR_AUX(x) #x
 #    define __LV_TO_STR(x) __LV_TO_STR_AUX(x)
@@ -2167,6 +2168,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h" */
 
 /*If running without lv_conf.h add typdesf with default value*/
 #if defined(LV_CONF_SKIP) || defined(CONFIG_LV_CONF_SKIP)
+
   /* Type of coordinates. Should be `int16_t` (or `int32_t` for extreme cases) */
   typedef int16_t lv_coord_t;
 
